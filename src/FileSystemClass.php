1166bf9ef840d03110d65aa935d43508d5cf171a
dallas-frankel <dallasfrankel@outlook.com>
2020-11-27 19:12:59 +0000

    Filled in simple methods for FileSystemClass

--- src/FileSystemClass.php
+++ src/FileSystemClass.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace Tsc\CatStorageSystem;
-
-class FileSystem
+require_once('DirectoryInterface.php');
+require_once('FileInterface.php');
+require_once('FileSystemInterface.php');
+class FileSystem implements FileSystemInterface
 {
 
   public function createFile(FileInterface $file, DirectoryInterface $parent){
-   
+    $parent->addToFiles($file);
   }
 
   public function updateFile(FileInterface $file){
-
+    $file->setModifiedTime(new \DateTime());
   }
 
   public function renameFile(FileInterface $file, $newName){
@@ -26,9 +28,9 @@ class FileSystem
   }
 
   public function createDirectory(DirectoryInterface $directory, DirectoryInterface $parent){
-     //Checks if name will be a duplicate in that file system
+     //Checks if name will be a duplicate in that file system 
     if(!$parent->checkForNameInDirectories($directory->getName())){
-      $directory->setName($directory->getName() . "1");
+      $directory->setName($directory->getName() . "(1)");
     }
   }
 
@@ -44,26 +46,25 @@ class FileSystem
 
   
   public function getDirectoryCount(DirectoryInterface $directory){
-
+    return count($directory->getDirectories());
   }
 
   
   public function getFileCount(DirectoryInterface $directory){
-
+    return count($directory->getFiles());
   }
 
   
   public function getDirectorySize(DirectoryInterface $directory){
-
+   
   }
 
   
   public function getDirectories(DirectoryInterface $directory){
-
+    return $directory->getDirectories();
   }
 
-
   public function getFiles(DirectoryInterface $directory){
-
+    return $directory->getFiles();
   }
 }
