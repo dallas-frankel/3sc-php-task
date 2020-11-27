c551182b1d46b0830d7c444405004b98e98b88b9
dallas-frankel <dallasfrankel@outlook.com>
2020-11-27 22:15:40 +0000

    Modified testing controller
    Changed controller to accomadate new directory constructor

--- src/Controller.php
+++ src/Controller.php
@@ -6,12 +6,12 @@ require_once('FileSystemClass.php');
 require_once('DirectoryClass.php');
 require_once('FileClass.php');
 
-$fileSystem = new FileSystemClass();
-$rootDirectory = new DirectoryClass("RootDir",null);
-$baseDirectory = new DirectoryClass("Base1",$rootDirectory);
-$base2Directory = new DirectoryClass("Base2",$rootDirectory);
-$baseChildDirectory = new DirectoryClass("Base1Child",$baseDirectory);
-$baseChildFile = new File("Base1File",54);
+$fileSystem = new FileSystem();
+$rootDirectory = new Directory("RootDir");
+$baseDirectory = new Directory("baseDirectory");
+$base2Directory = new Directory("base2Directory");
+$baseChildDirectory = new Directory("baseChildDirectory");
+$baseChildFile = new File("Base1File",1);
 
 
 $fileSystem->createRootDirectory($rootDirectory);
@@ -19,8 +19,16 @@ $fileSystem->createDirectory($baseDirectory,$rootDirectory);
 $fileSystem->createDirectory($base2Directory,$rootDirectory);
 $fileSystem->createDirectory($baseChildDirectory,$baseDirectory);
 $fileSystem->createFile($baseChildFile,$baseDirectory);
+$fileSystem->createFile($baseChildFile,$baseDirectory);
+$fileSystem->createFile($baseChildFile,$rootDirectory);
+$fileSystem->createFile($baseChildFile,$base2Directory);
+$fileSystem->createFile($baseChildFile,$base2Directory);
 
-echo $fileSystem->getDirectorySize($rootDirectory);
 
+
+$hello = $fileSystem->getDirectorySize($rootDirectory);
+echo '<br>';
+echo  "Size is " . $hello;
+//$fileSystem->getFiles($baseDirectory);
 
 ?>
